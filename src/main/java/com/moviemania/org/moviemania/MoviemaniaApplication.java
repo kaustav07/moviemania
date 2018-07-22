753179ca39b37d8f67b8506a47629aac77ea0516
Kaustav <chatterjeekaustav11@gmail.com>
2018-07-23 01:05:31 +0530

    Initial commit

---
+++ src/main/java/com/moviemania/org/moviemania/MoviemaniaApplication.java
@@ -0,0 +1,12 @@
+package com.moviemania.org.moviemania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviemaniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviemaniaApplication.class, args);
+	}
+}
